587b42fe21a8ed90f1cb40d00c04dbfae3f668f2
bell1002 <hothikieulinh17@gmail.com>
2024-03-28 23:56:34 +0700

    two day

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -25,9 +25,13 @@
                     <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
                         <div class="card card-primary border-box">
                             <div class="card-header card-header-auth">
+                           
                                 <h4 class="text-center">Admin Panel Login</h4>
                             </div>
                             <div class="card-body card-body-auth">
+                                @if(session()->get('success'))
+                                    <div class="text-success">{{ session()-> get('success')}}</div>
+                                @endif
                                 <form method="POST" action="{{ route('admin_login_submit') }}">
                                     @csrf
                                     <div class="form-group">
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@ Route::get('/admin/login',[AdminLoginController::class, 'index'])->name('admin_l
 Route::post('/admin/login-submit',[AdminLoginController::class,'login_submit'])->name('admin_login_submit');
 Route::get('/admin/forget-password',[AdminLoginController::class, 'forget_password'])->name('admin_forget_password');
 Route::get('/admin/logout',[AdminLoginController::class, 'logout'])->name('admin_logout');
+Route::post('/admin/forget-password-submit',[AdminLoginController::class,'forget_password_submit'])->name('admin_forget_password_submit');
